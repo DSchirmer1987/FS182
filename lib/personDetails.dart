c5f48bdcb4cfc7a0486ab213e8011bbfde002235
Daniel Schirmer <d.schirmer1987@gmail.com>
2021-01-08 08:55:33 +0100

    --- Auto-Git Commit ---

--- lib/personDetails.dart
+++ lib/personDetails.dart
@@ -14,6 +14,7 @@ class PersonDetails extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    print(Platform.localeName);
     return Scaffold(
       appBar: AppBar(
         title: Text("Person Details"),
