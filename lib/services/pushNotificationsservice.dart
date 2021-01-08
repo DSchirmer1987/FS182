603a5cab7fdeb84f41c210f6eec2c43b83fa8ac4
Daniel Schirmer <d.schirmer1987@gmail.com>
2021-01-08 10:12:54 +0100

    --- Auto-Git Commit ---

--- lib/services/pushNotificationsservice.dart
+++ lib/services/pushNotificationsservice.dart
@@ -1,5 +1,30 @@
+import 'dart:io';
+
 import 'package:firebase_messaging/firebase_messaging.dart';
 
 class PushNotificationService{
-  final FireBas
+  final FirebaseMessaging _fcm;
+
+  PushNotificationService(this._fcm);
+
+  Future initialise() async{
+    if(Platform.isIOS){
+      _fcm.requestNotificationPermissions(IosNotificationSettings());
+    }
+
+    String token = await _fcm.getToken();
+    print("FCM token: $token");
+
+    _fcm.configure(
+      onMessage: (Map<String, dynamic> message) async{
+        print("onMessage: $message");
+      },
+      onLaunch: (Map<String, dynamic> message) async{
+        print("onLaunch: $message");
+      },
+      onResume: (Map<String, dynamic> message) async{
+        print("onResume: $message");
+      },
+    );
+  }
 }
